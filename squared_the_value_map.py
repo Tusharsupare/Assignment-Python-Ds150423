64fc7cf678ad1b99b07da144627fd36d6d4aa4cb
Tushar <92177191+Tusharsupare@users.noreply.github.com>
2023-05-26 12:23:10 +0530

    squared_the_value_map.py

--- squared_the_value_map.py
+++ squared_the_value_map.py
@@ -1,16 +1,3 @@
-# def square(num):
-#     return num ** 2
-
-# # Sample List
-# my_list = [4, 5, 2, 9]
-
-# # Using map() to square the elements of the list
-# squared_list = list(map(square, my_list))
-
-# print("Original List:", my_list)
-# print("Squared List:", squared_list)
-
-
 def square_root(number):
     return number ** 2
 
