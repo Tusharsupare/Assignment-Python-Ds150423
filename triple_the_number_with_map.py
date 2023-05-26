afa4162d4cdbf22d0a2ced8362e9ec4f9a3816bf
Tushar <92177191+Tusharsupare@users.noreply.github.com>
2023-05-26 12:20:04 +0530

    Add files via upload

---
+++ triple_the_number_with_map.py
@@ -0,0 +1,14 @@
+# def triple_numbers(numbers):
+#     return list(map(lambda x: x * 3, numbers))
+
+# sample_list = [1, 2, 3, 4, 5, 6, 7]
+# result = triple_numbers(sample_list)
+# print(result)
+
+# Using Function
+def triple_no(numbers):
+    return list(map(lambda x: x * 3, numbers))
+
+sample_lst = [1, 2, 3, 4, 5, 6, 7]    
+result = triple_no(sample_lst)
+print(f"The value with triple number ---> {result}")
