36c534a741b72c3346554e09c8a801d14a964d4e
Tushar <92177191+Tusharsupare@users.noreply.github.com>
2023-06-01 14:19:06 +0530

    task_4_implement_the_banking_acc.py

--- task_4_implement_the_banking_acc.py
+++ task_4_implement_the_banking_acc.py
@@ -7,9 +7,9 @@ class Account:
 class SavingsAccount(Account):
     def __init__(self, title, balance, interestRate):
         super().__init__(title, balance)
-        self.interestRate = 5
+        self.interestRate = intrestRate
 
-account = SavingsAccount("ashish", 500, 5)
+account = SavingsAccount("ashish", 500)
 print(account.title)  
 print(account.balance)  
 print(account.interestRate)  
