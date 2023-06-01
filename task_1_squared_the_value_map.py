d90042137ecfd6adff3bb9a7fc14b101e11d20b6
Tushar <92177191+Tusharsupare@users.noreply.github.com>
2023-06-01 14:13:01 +0530

    Add files via upload

---
+++ task_1_squared_the_value_map.py
@@ -0,0 +1,10 @@
+# def square_root(number):
+#     return number ** 2
+
+# # Given Sample List    
+# given_list = [4, 5, 2, 9]
+
+# # using map() to get squared numbers
+# squared_list = list(map(square_root,given_list))
+# print("Original List:", given_list)
+# print("Squared List:", squared_list)
