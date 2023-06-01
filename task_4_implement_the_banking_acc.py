9e6071926908d7dcadbfc84eda114de22872b7c7
Tushar <92177191+Tusharsupare@users.noreply.github.com>
2023-06-01 14:14:14 +0530

    Add files via upload

---
+++ task_4_implement_the_banking_acc.py
@@ -0,0 +1,17 @@
+class Account:
+    def __init__(self, title, balance):
+        self.title = title
+        self.balance = balance
+        self.property = None  
+
+class SavingsAccount(Account):
+    def __init__(self, title, balance, interestRate):
+        super().__init__(title, balance)
+        self.interestRate = 5
+
+account = SavingsAccount("ashish", 500, 5)
+print(account.title)  
+print(account.balance)  
+print(account.interestRate)  
+
+print(f"Account Holder : {account.title}\nAccount Balance : {account.balance}\nIntrestRate : {account.interestRate}")
